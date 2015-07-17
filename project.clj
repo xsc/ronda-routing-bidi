cfb95b7769d4ed2471e5271e732490f067760c63
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-07-17 12:20:28 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "https://github.com/xsc/ronda-routing-bidi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.reader "0.9.2"]
                  [ronda/routing "0.2.7"]
-                 [clj-time "0.9.0"]
-                 [bidi "1.18.10"
+                 [clj-time "0.10.0"]
+                 [bidi "1.20.1"
                   :exclusions [org.clojure/clojurescript
                                com.keminglabs/cljx
                                commons-fileupload]]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [joda-time "2.7"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]
+                                  [joda-time "2.8.1"]]
                    :plugins [[lein-midje "3.1.3"]]}}
   :aliases {"test" ["midje"]}
   :pedantic? :abort)
